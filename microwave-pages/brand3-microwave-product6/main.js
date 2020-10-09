33d25c0c4504998f6a5bc30a77dc4744611ef969
mohamed552 <50499478+mohamed552@users.noreply.github.com>
2020-10-09 16:58:17 +0200

    Files upload

---
+++ microwave-pages/brand3-microwave-product6/main.js
@@ -0,0 +1,8 @@
+function show_cart() {
+  document.getElementById("frame").style.display = "block";
+  document.getElementById("sub-gallery").style.display = "none";
+}
+function hide_cart() {
+  document.getElementById("frame").style.display = "none";
+  document.getElementById("sub-gallery").style.display = "block";
+}
